e7c7bcd468e277c46d1c3ffd518ccd6a7e553ead
bluelovers <codelovers@users.sourceforge.net>
2018-04-03 19:45:16 +0800

    .

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,45 @@
-declare function sortObject<T>(object: T, options?: sortObject.IOptions & {
-    useSource: true;
-}): T;
-declare function sortObject<T>(object: T, options?: sortObject.IOptions & {
-    keys: string[];
-    onlyKeys: true;
-}): Partial<T>;
-declare function sortObject<T>(object: T, options?: sortObject.IOptions): Partial<T>;
-declare function sortObject<T>(object: T, sortFn: (a, b) => any): Partial<T>;
-declare function sortObject<T>(object: T, sortWith: string[]): Partial<T>;
-declare module sortObject {
-    interface IOptions {
-        /**
-         * key order
-         */
-        keys?: string[];
-        /**
-         * return Object only keys
-         * will disable useSource
-         */
-        onlyKeys?: boolean;
-        /**
-         * sort callback
-         *
-         * @param a
-         * @param b
-         * @returns {any}
-         */
-        sort?: (a, b) => any;
-        /**
-         * return reversed Object
-         */
-        desc?: boolean;
-        allowNotExists?: boolean;
-        /**
-         * return source Object
-         */
-        useSource?: boolean;
-    }
+/**
+ * Created by user on 2018/4/3/003.
+ */
+import * as sortObject from './core';
+declare const sortObjectKeys: {
+    <T>(object: T, options?: sortObject.IOptions & {
+        useSource: true;
+    }): T;
+    <T>(object: T, options?: sortObject.IOptions & {
+        keys: string[];
+        onlyKeys: true;
+    }): Partial<T>;
+    <T>(object: T, options?: sortObject.IOptions): Partial<T>;
+    <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
+    <T>(object: T, sortWith: string[]): Partial<T>;
+} & {
+    default: {
+        <T>(object: T, options?: sortObject.IOptions & {
+            useSource: true;
+        }): T;
+        <T>(object: T, options?: sortObject.IOptions & {
+            keys: string[];
+            onlyKeys: true;
+        }): Partial<T>;
+        <T>(object: T, options?: sortObject.IOptions): Partial<T>;
+        <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
+        <T>(object: T, sortWith: string[]): Partial<T>;
+    };
+    sortObjectKeys: {
+        <T>(object: T, options?: sortObject.IOptions & {
+            useSource: true;
+        }): T;
+        <T>(object: T, options?: sortObject.IOptions & {
+            keys: string[];
+            onlyKeys: true;
+        }): Partial<T>;
+        <T>(object: T, options?: sortObject.IOptions): Partial<T>;
+        <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
+        <T>(object: T, sortWith: string[]): Partial<T>;
+    };
+};
+declare namespace sortObjectKeys {
+    type IOptions = sortObject.IOptions;
 }
-export = sortObject;
+export = sortObjectKeys;
