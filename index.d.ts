5b0f410c454e1ee234483cb82aad8ab76cd72a37
bluelovers <codelovers@users.sourceforge.net>
2018-04-19 05:17:49 +0800

    .

--- index.d.ts
+++ index.d.ts
@@ -2,42 +2,9 @@
  * Created by user on 2018/4/3/003.
  */
 import * as sortObject from './core';
-declare const sortObjectKeys: {
-    <T>(object: T, options?: sortObject.IOptions & {
-        useSource: true;
-    }): T;
-    <T>(object: T, options?: sortObject.IOptions & {
-        keys: string[];
-        onlyKeys: true;
-    }): Partial<T>;
-    <T>(object: T, options?: sortObject.IOptions): Partial<T>;
-    <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
-    <T>(object: T, sortWith: string[]): Partial<T>;
-} & {
-    default: {
-        <T>(object: T, options?: sortObject.IOptions & {
-            useSource: true;
-        }): T;
-        <T>(object: T, options?: sortObject.IOptions & {
-            keys: string[];
-            onlyKeys: true;
-        }): Partial<T>;
-        <T>(object: T, options?: sortObject.IOptions): Partial<T>;
-        <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
-        <T>(object: T, sortWith: string[]): Partial<T>;
-    };
-    sortObjectKeys: {
-        <T>(object: T, options?: sortObject.IOptions & {
-            useSource: true;
-        }): T;
-        <T>(object: T, options?: sortObject.IOptions & {
-            keys: string[];
-            onlyKeys: true;
-        }): Partial<T>;
-        <T>(object: T, options?: sortObject.IOptions): Partial<T>;
-        <T>(object: T, sortFn: (a: any, b: any) => any): Partial<T>;
-        <T>(object: T, sortWith: string[]): Partial<T>;
-    };
+declare const sortObjectKeys: typeof sortObject & {
+    default: typeof sortObject;
+    sortObjectKeys: typeof sortObject;
 };
 declare namespace sortObjectKeys {
     type IOptions = sortObject.IOptions;
