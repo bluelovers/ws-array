44a586363f01f66ac4d061b71bdefd0a17d4d2b6
bluelovers <codelovers@users.sourceforge.net>
2020-06-20 01:32:11 +0800

    fix(typescript): avoid https://github.com/microsoft/TypeScript/issues/39149

--- packages/string-natural-compare/core.js
+++ packages/string-natural-compare/core.js
@@ -1,59 +1,19 @@
 "use strict";
-/**
- * Created by user on 2020/6/4.
- */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.compareCaseInsensitive = exports.createNew = exports.naturalCompare = void 0;
-const string_natural_compare_1 = __importDefault(require("string-natural-compare"));
-/**
- * Compare alphanumeric strings the same way a human would,
- * using a natural order algorithm
- * (originally known as the alphanum algorithm)
- * where numeric characters are sorted
- * based on their numeric values rather than their ASCII values.
- */
-function naturalCompare(a, b, opts) {
-    let i;
-    if (typeof a === 'number' && typeof b === 'number') {
-        i = a - b;
-    }
-    else {
-        if (typeof a === 'number') {
-            a = String(a);
-        }
-        else if (typeof b === 'number') {
-            b = String(b);
-        }
-        if (a === b) {
-            return 0;
-        }
-        i = string_natural_compare_1.default(a, b, opts);
-    }
-    if (i !== 0 && (opts === null || opts === void 0 ? void 0 : opts.desc)) {
-        i = 0 - i;
-    }
-    return i;
-}
-exports.naturalCompare = naturalCompare;
-/**
- * create compare with preset options
- */
-function createNew(opts) {
-    return (a, b) => naturalCompare(a, b, opts);
-}
-exports.createNew = createNew;
-/**
- * compare strings case-insensitively
- */
-exports.compareCaseInsensitive = createNew({
-    caseInsensitive: true,
-});
-naturalCompare.createNew = createNew;
-naturalCompare.compareCaseInsensitive = exports.compareCaseInsensitive;
-naturalCompare.default = naturalCompare;
-Object.defineProperty(naturalCompare, "__esModule", { value: true });
-exports.default = naturalCompare;
+__exportStar(require("./index"), exports);
+const index_1 = __importDefault(require("./index"));
+exports.default = index_1.default;
 //# sourceMappingURL=core.js.map
